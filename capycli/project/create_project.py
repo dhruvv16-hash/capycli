8e34cbcc6b046e02e91627599f8c22512b93363f
unknown <dhruv.vira16@gmail.com>
2026-09-11 00:17:37 +0530

    Fix unreachable code in mypy check

--- capycli/project/create_project.py
+++ capycli/project/create_project.py
@@ -146,11 +146,11 @@ class CreateProject(capycli.common.script_base.ScriptBase):
             if swex.response.status_code == requests.codes["forbidden"]:
                 print_red("  You are not authorized - do you have a valid write token?")
                 sys.exit(ResultCode.RESULT_AUTH_ERROR)
-            if swex.response is not None:
-                print_red("  " + str(swex.response.status_code) + ": " + swex.response.text)
-                sys.exit(ResultCode.RESULT_ERROR_ACCESSING_SW360)
             if swex.details:
                 print_red("  " + swex.details.get("error", "") + ": " + swex.details.get("message", ""))
+                sys.exit(ResultCode.RESULT_ERROR_ACCESSING_SW360)
+            if swex.response.text:
+                print_red("  " + str(swex.response.status_code) + ": " + swex.response.text)
                 sys.exit(ResultCode.RESULT_ERROR_ACCESSING_SW360)
 
             print_red("  Unknown error updating project: " + repr(swex))
